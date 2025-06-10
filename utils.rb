f668cedac9146fff357a7108a91dc3df9a18e3f4
olubori <daveholuborhee@gmail.com>
2025-06-10 21:36:38 +0100

    fix indentation

--- utils.rb
+++ utils.rb
@@ -1,23 +1,23 @@
 module PricingRules
-    DELIVERY_RULE = ->(subtotal) do
-      if subtotal < 50
-        4.95
-      elsif subtotal < 90
-        2.95
-      else
-        0.0
-      end
+  DELIVERY_RULE = ->(subtotal) do
+    if subtotal < 50
+      4.95
+    elsif subtotal < 90
+      2.95
+    else
+      0.0
     end
   end
+end
   
-  module ProductCodes
-    RED   = 'R01'
-    GREEN = 'G01'
-    BLUE  = 'B01'
-  
-    ALL = {
-      red: RED,
-      green: GREEN,
-      blue: BLUE
-    }
-  end
+module ProductCodes
+  RED   = 'R01'
+  GREEN = 'G01'
+  BLUE  = 'B01'
+
+  ALL = {
+    red: RED,
+    green: GREEN,
+    blue: BLUE
+  }
+end
